6630f8ccde3296403f6dd535e04d5d646b33c22a
SanderML <luca.sander@online.de>
2021-11-30 18:02:58 +0100

    adding Deck module

--- deck_of_cards/_nbdev.py
+++ deck_of_cards/_nbdev.py
@@ -3,10 +3,11 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Card": "00_card.ipynb",
-         "Deck": "00_card.ipynb",
-         "Hand": "00_card.ipynb"}
+         "Deck": "01_deck.ipynb",
+         "Hand": "01_deck.ipynb"}
 
-modules = ["card.py"]
+modules = ["card.py",
+           "deck.py"]
 
 doc_url = "https://SanderML.github.io/deck_of_cards/"
 
